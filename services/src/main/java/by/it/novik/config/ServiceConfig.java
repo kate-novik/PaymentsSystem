fed9683730a10822af2f8c231e0799f64ec8ecfe
kate-novik <ekat.novik@gmail.com>
2016-09-17 17:20:23 +0300

    Min update

--- services/src/main/java/by/it/novik/config/ServiceConfig.java
+++ services/src/main/java/by/it/novik/config/ServiceConfig.java
@@ -12,31 +12,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
  */
 @Configuration
 @ComponentScan("by.it.novik.services")
-@Import({ HibernateConfig.class})
+@Import({HibernateConfig.class})
 public class ServiceConfig {
 
     @Bean
     public UserDetailsService userDetailsService(){
         return new UserDetailsServiceImpl();
     }
-
-//    @Bean
-//    public UserService userService () {
-//        return new UserService();
-//    }
-//
-//    @Bean
-//    public AccountService accountService () {
-//        return new AccountService();
-//    }
-//
-//    @Bean
-//    public PaymentService paymentService () {
-//        return new PaymentService();
-//    }
-//
-//    @Bean
-//    public RoleService roleService() {
-//        return new RoleService();
-//    }
 }
